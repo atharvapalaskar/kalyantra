7ab88513bc886f8abaefdf9ef128aeb776809366
Atharva Palaskar <atharvapalaskar@gmail.com>
2022-12-31 02:24:07 +0530

    app(late-commit): ui_updates, speech to text, voice_bot_moves

--- app/lib/enums.dart
+++ app/lib/enums.dart
@@ -11,3 +11,13 @@ enum BotStatus {
   notReady,
   ok
 }
+
+class BotMoves {
+   String get halt => 'halt';
+   String get forward => 'forward';
+   String get backward => 'backward';
+   String get rightfront => 'right-front';
+   String get rightback => 'right-back';
+   String get leftfront => 'left-front';
+   String get leftback => 'left-back';
+}
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,8 +2,11 @@ import 'dart:convert';
 
 import 'package:app/components.dart';
 import 'package:app/enums.dart';
-import 'package:flutter/material.dart'; 
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart'; 
 import 'package:http/http.dart' as http;
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 import 'env.dart';
 void main() {
@@ -15,9 +18,10 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { 
     return MaterialApp(
       theme: ThemeData( 
+        fontFamily: GoogleFonts.pressStart2p().fontFamily,
         primarySwatch: Colors.amber,
         backgroundColor: Colors.black,  
       ),
@@ -34,10 +38,67 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+ 
+  // speech
+  SpeechToText _speechToText = SpeechToText();
+  bool _speechEnabled = false;
+  String recWords = '--';String lastrecWords = '--';
+
+  @override
+  void initState() {
+    super.initState();
+    _initSpeech();
+  }
+ 
+  void _initSpeech() async {
+    _speechEnabled = await _speechToText.initialize(
+      onStatus: (status) {
+        switch (status) {
+          case 'listening':
+            setState(() { recWords = 'listening...';}); 
+            break;
+          case 'done':
+             _onDone();
+            break;
+        } 
+      },
+    );
+    setState(() {});
+  }
+ 
+  void _startListening() async {
+    setState(() {lastrecWords = recWords;});
+    await _speechToText.listen(onResult: _onSpeechResult);
+    setState(() {});
+  }
+ 
+  void _stopListening() async {
+    await _speechToText.stop();
+    setState((){});
+  }
+ 
+  void _onSpeechResult(SpeechRecognitionResult result) { 
+    setState(() { recWords = result.recognizedWords; });
+    print('recWr $recWords');
+  }
   
+  void _onDone(){
+     print('done');
+     if(recWords.startsWith('move')){
+        print('movess');
+        print(recWords.split(' ')[1]);
+        print(recWords.split(' ').length > 2);
+        move(recWords.split(' ').length > 2? '${recWords.split(' ')[1]}-${recWords.split(' ')[2]}' : recWords.split(' ')[1] );
+     }
+  }
+
+  //kalyantra apis
   String? ngrokurl;
   NgConnectionState connectionState = NgConnectionState.disconnected;
+
   BotStatus botStatus = BotStatus.notReady;
+  String botMove = BotMoves().halt;
+  String lastbotMove = '--';
 
   Future<void> connect() async {
     setState(() {connectionState = NgConnectionState.connecting;});
@@ -62,9 +123,10 @@ class _HomeScreenState extends State<HomeScreen> {
     } 
   }
 
-  Future<void> move( String _movee) async {
+  Future<void> move( String _movee) async { 
      try { var res = await http.post(Uri.parse('$ngrokurl/moves/$_movee')); 
-     if(json.decode(res.body)['msg']!='ok'){throw 'cant move';}
+      if(json.decode(res.body)['msg']!='ok'){throw 'cant move';}
+      setState(() {lastbotMove = botMove; botMove = _movee; });
      } catch(e) { print(e); setState(() {connectionState = NgConnectionState.disconnected;});  }
    
   }
@@ -88,19 +150,33 @@ class _HomeScreenState extends State<HomeScreen> {
      return Scaffold(
       backgroundColor: Colors.black,
       appBar: PreferredSize(  preferredSize: const Size.fromHeight(60.0),
-          child:SafeArea( child: Container(height: 60,color: Colors.amber,child:const Center(child: Text('Kalyantra: Well-being bot',style: TextStyle(fontSize: 16,color: Colors.black, fontWeight: FontWeight.w700,) )),) ),
+          child:SafeArea( child: Container(height: 60,color: Colors.amber,child:const Center(child: Text('Kalyantra: Well-being bot',style: TextStyle(fontSize: 12,color: Colors.black, fontWeight: FontWeight.w700,) )),) ),
       ),
-      body: Center( child: 
+      body: Padding(padding: const EdgeInsets.all(30), child: 
        
           Stack(children: [
             
-            MovesBox(move),
+            Column( mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+                
+                RichText( maxLines: 3,text: TextSpan(style: TextStyle(color: Colors.white,fontSize: 12,letterSpacing: 0.4,height: 2.6,fontFamily: GoogleFonts.pressStart2p().fontFamily),
+                  children:[ 
+                    TextSpan(text: 'Bot Move : $botMove',style:const TextStyle(color: Colors.amber,)),
+                    TextSpan(text: '\n Last Move : $lastbotMove',style:const TextStyle(fontSize: 10,)),                  
+                ])),
+                
+                MovesBox(move),
+
+                Align(alignment: Alignment.centerLeft,child: Text('Voice cmd: $recWords',style:const TextStyle(color: Colors.amber,fontSize: 10,letterSpacing: 0.4,height: 1.6,),textAlign: TextAlign.left,)),
+                Align(alignment: Alignment.centerLeft,child: Text('Last V-cmd: $lastrecWords ',style:const TextStyle(color: Colors.white,fontSize: 8,height: 1.6,),textAlign: TextAlign.left,)),
+             
+                // const SizedBox(height: 1,)
+              ],),
 
             connectionState != NgConnectionState.connected ?
             Container(width: MediaQuery.of(context).size.width,color: Colors.black.withOpacity(0.86),
              child:  
               
-              connectionState == NgConnectionState.connecting?
+               connectionState == NgConnectionState.connecting?
 
                Column(crossAxisAlignment: CrossAxisAlignment.center,mainAxisAlignment: MainAxisAlignment.center,
                 children: const [
@@ -120,7 +196,15 @@ class _HomeScreenState extends State<HomeScreen> {
 
           ],),
       
-       )
+       ),
+       floatingActionButton: FloatingActionButton( 
+        onPressed:
+            // If not yet listening for speech start, otherwise stop
+            _speechToText.isNotListening ? _startListening : _stopListening,
+        tooltip: 'Listen',backgroundColor:  connectionState != NgConnectionState.connected ? Colors.amber.withOpacity(0.3) : Colors.amber,
+        child: Icon(_speechToText.isNotListening ? Icons.mic_off : Icons.mic),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniCenterFloat,
     );
   }
 }
